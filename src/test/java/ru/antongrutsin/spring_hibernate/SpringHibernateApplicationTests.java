36c2b7e1349d892ca7f8188f2016b47e46e03668
antongrutsin <Risenka13524>
2021-04-25 16:15:06 +0300

    Initial commit

---
+++ src/test/java/ru/antongrutsin/spring_hibernate/SpringHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.antongrutsin.spring_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
